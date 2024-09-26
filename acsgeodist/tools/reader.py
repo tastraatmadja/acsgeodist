81cb33f8568084541cefd5f0f12daadbb12c74d6
Tri Astraatmadja <trilaksmana@gmail.com>
2024-09-25 20:12:39 -0400

    Some corrections

---
+++ acsgeodist/tools/reader.py
@@ -0,0 +1,18 @@
+from acsgeodist.tools import sip
+from acsgeodist import acsconstants
+import pandas as pd
+def readFitResults(fitResultsFilename, pOrder):
+    columns = ['rootname', 'chip', 'time', 'pa_v3', 'orientat', 'vaFactor', 'tExp', 'posTarg1', 'posTarg2',
+               'nIterTotal', 'nStars', 'rmsXi', 'rmsEta', 'crval1', 'crval2', 'cd11', 'cd12', 'cd21', 'cd22']
+
+    n = pOrder + 1
+
+    nParsAxis = sip.getUpperTriangularMatrixNumberOfElements(n)
+
+    for ppp in range(nParsAxis):
+        for axis in range(acsconstants.NAXIS):
+            coeffName = '{0:s}_{1:d}'.format(acsconstants.COEFF_LABELS[axis], ppp + 1)
+
+            columns.append(coeffName)
+
+    return pd.read_csv(fitResultsFilename, sep='\s+', header=None, comment='#', names=columns)
