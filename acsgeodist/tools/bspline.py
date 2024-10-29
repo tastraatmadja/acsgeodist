744701e3b3269d326759582305c6c2ec9b57133c
Tri L. Astraatmadja <tastraatmadja@stsci.edu>
2024-10-29 00:34:57 -0400

    Updating B-spline basis function calculation using scipy.interpolate.BSpline in scipy 1.14.1

--- acsgeodist/tools/bspline.py
+++ acsgeodist/tools/bspline.py
@@ -1,20 +1,28 @@
 import numpy as np
-from scipy import signal
+from scipy import interpolate
 
 def getForwardModelBSpline(x, kOrder, knots):
     ## Number of coefficients based on the knot points and the order the B-spline
-    nParsT = knots.size + kOrder  ## Number of parameters include constant parameter (zero point)
+    nParsT = knots.size + kOrder  ## Number of parameters INCLUDES constant parameter (zero point)
 
     h = knots[1] - knots[0]
 
-    xMin = knots[0]
+    nSplines = knots.size + kOrder - 1
+    tKnotSpl = np.linspace(knots[0] - kOrder * h, knots[-1] + kOrder * h, knots.size + 2 * kOrder, endpoint=True)
 
     ## Forward model
     X = np.zeros((x.size, nParsT))
 
     X[:, 0] = 1.0
+
     for i in range(1, nParsT):
-        knotID = i - kOrder - 1
-        X[:, i] = signal.bspline((x - xMin) / h - 0.5 * (kOrder + 1) - knotID, kOrder)
+        start = i - 1
+        end = start + kOrder + 2
+
+        bspline = interpolate.BSpline.basis_element(tKnotSpl[start:end], extrapolate=False)
+
+        X[:, i] = bspline(x)
+
+    X[np.isnan(X)] = 0.0
 
     return X
